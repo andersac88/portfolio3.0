5b174d2d4462f082c77702130293f265ecc78fdb
Andrew Andersen <andersac88@gmail.com>
2020-10-03 20:21:52 -0500

    updates

---
+++ src/components/Blog/blog.js
@@ -0,0 +1,29 @@
+const blogPosts = [
+	{
+		Date: 'July 10, 2020',
+		Text: `<p>I was recently listening to The Dave Ramsey podcast when made reference to the quote by author and motivational speaker Simon Sinek, “Working hard for something we don't care about is called stress; working hard for something we love is called passion.”</p>
+    <p>I have since thought hard on this quote and have found it very poignant.</p>
+    <p>I feel this way not only because I am unfulfilled in my current occupation but because I know there are millions of others who feel as I do but must continue the grind in order to provide for themselves and/or their families.</p>
+    <p>I dream of the day where my passion becomes my career.</p>`,
+	},
+	{
+		Date: 'July 9, 2020',
+		Text: `<p>I find myself once again reflecting on all that is happening in the world and realize the importance of actually knowing history. It is becoming far too common for people, especially those in places of power, to misrepresent events in the past or make claims that directly contradict history to support their viewpoints.</p>
+    <p>One instance that comes to mind is a recent tweet by Representative Thomas Massie out of Kentucky. He tweeted,</p>
+    <p style="text-align: center; font-size: 18px;">"There is no authority in the Constitution that authorizes the government to stick a needle in you against your will, force you to wear a face mask, or track your daily movements.<br> 🇺🇸 <br> Can you imagine the signers of the Declaration of Independence submitting to any of these things?!"</p>
+    <p>While his interpretation of the Constitution is patently incorrect; see Jacobson v. Massachusetts, 197 U.S. 11 (1905). I take issue with the latter half of his dimwitted tweet, primarily because I consider myself a history buff and not a legal scholar.</p>
+    <p>Fortunately, there are others in the Twitterverse who also completed middle-school and pointed out that in 1777 General George Washington ordered that the Continental Army be inoculated against smallpox, arguaby aiding in victory over the British.</p>
+    <p>Now, I don't care that Massie is so ignorant, though I question if an MIT grad is infact this ill-informed or simply being divisive; my problem is that as a US Representative, he has influence and there are people who will uses his statements as justification for not heeding the advice of the actual experts.</p>`,
+	},
+	{
+		Date: 'June 30, 2020',
+		Text: `<p>I have never written a blog before but in light of recent events I feel compelled to write down my thoughts. I hope that this will assist in not only clearing my head, but also in reflecting on my perception of the world that surrounds me... I also hope that this doesn't end up as another webpage dedicated to unintelligible rantings. Let's begin...</p>
+    <p>I consider the reluctance to wear face masks to be one of the most absurd happenings in this eternity that is 2020.</p>
+    <p>Facts and science aside, basic common sense should be enough to convince someone that masks are beneficial in the fight against a global pandemic.</p>
+    <p>I understand that they are uncomfortable but that seems like a small sacrifice if it could in anyway contribute to preventing a needless death. I happened across a posting recently that worded my thoughts quite succinctly... "if the experts are right, wearing a mask could aid in preventing the spread of a deadly contagion. If the experts are wrong, you are wearing a piece of fabric across your face." Honestly, how has this become a discussion? </p></p>
+    <p>As for the ludicrous argument that masks are somehow an infringement of one's freedoms, the Supreme Court has already ruled that “liberty” under the Constitution does not give individuals the right to make decisions about their own health in instances when those decisions could endanger others.</p>
+    <p>I find solace in the fact that the majority think as I do on this subject. While I am also I live in a State, Illinois, who's response to COVID-19 has appeared effective, as of today, at "flattening the curve"</p>`,
+	},
+];
+
+module.exports = blogPosts;
