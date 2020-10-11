2601c280f0640ae316665b45fb76661ca08f7543
Andrew Andersen <andersac88@gmail.com>
2020-10-11 12:28:35 -0500

    remove maps

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Map from '../Map';
-
 const Contact = () => {
 	return (
 		<div>
 			<h1>Contact</h1>
 			<h1>Address</h1>
 			<h1>E-mail</h1>
-			<Map />
 		</div>
 	);
 };
