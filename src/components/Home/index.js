2466dce5c34c0217c5a2c69e030166f7b5f719cd
Andrew Andersen <andersac88@gmail.com>
2020-11-01 21:49:46 -0600

    updates

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -22,7 +22,8 @@ const Home = () => {
 			</h5>
 			<br />
 			<h5 style={ta}>
-				I'm also a political junkie with and keen interest in world history
+				I'm also a recovering political junkie with and keen interest in world
+				history
 			</h5>
 
 			<div className="banner">
