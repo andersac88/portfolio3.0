e9a85af5520de06718230cb65b2f4727cd5fbe44
Andrew Andersen <andersac88@gmail.com>
2020-11-15 20:56:51 -0600

    more

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-	faGithub,
-	faLinkedin,
-	faFacebook,
-} from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 import './style.css';
 import Simpsonized from '../../images/Simpsonized.jpg';
